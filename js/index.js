df6fa3ec2c59dbca67cec185236a5c6c53e5f4ef
nickwelsh <nwelsh@nicks-macbook-pro.local>
2016-09-06 15:05:26 -0400

    First Commit
    Should just work!

---
+++ js/index.js
@@ -0,0 +1,17 @@
+var app = angular.module('myApp', []);
+app.controller('MyAngularishCtrl',function ($scope, $interval) {
+  $scope.AngularishRNGesus = 0;
+  function update() {
+    $scope.AngularishRNGesus = Math.round((Math.random() * 10) * 10);
+  }
+  $interval(update, 1000 * 60);
+});
+
+
+var doughnut = 0;
+     window.setInterval(
+     function () {
+         doughnut = doughnut + Math.round((Math.random() * 10) * 10);
+         document.getElementById("doughnuts").innerHTML = "You have used " + doughnut + " staples in total";
+
+     }, 1000 * 60);
